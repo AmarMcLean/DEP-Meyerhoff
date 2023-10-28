0cda1ac865283544495a5b35d664c6494d65103e
AmarMcLean <114834915+AmarMcLean@users.noreply.github.com>
2023-10-28 17:51:14 -0400

    10/24/23

--- src/GUI.py
+++ src/GUI.py
@@ -1,32 +1,91 @@
-# GUY.py
+# GUI.py
 from tkinter import *
 from tkinter import filedialog
-import excel_class as excel
+import pandas as pd
+from pandastable import Table
+
 
 root = Tk()
 root.title("Data Entry Portal")
 root.state("zoomed")
 root.geometry("1920x1080")
 root.colors = ["#F8F4E3", "#D4CDC3", "#D5D0CD", "#A2A392", "#9A998C"]
+root.df = None
+checkboxes_view = []
+checkboxes_split = []
+searchbars = []
+root.file_path = None
 
 root.configure(bg=root.colors[0])
 
 
+#
+#
+#
+#
+# File tab and the load button and save button
 def file_click():
     # Disable search features
     if btnSearch.cget("state") == "disabled":
-        placeholder.grid_forget()
         btnSearch.config(state="normal")
+        top_row_frame_2.grid_forget()
     # Disable reformat features
     if btnReformat.cget("state") == "disabled":
         btnView.grid_forget()
+        btnSplit.grid_forget()
         btnCreate.grid_forget()
+        top_row_frame.grid_forget()
         btnReformat.config(state="normal")
     btnLoad.grid(row=0, column=0)
     btnSave.grid(row=1, column=0)
     btnFile.config(state="disabled")
 
 
+def load_click():
+    root.file_path = filedialog.askopenfilename(initialdir="/Documents", title="Select an Excel File")
+
+    if root.file_path:
+        # Load the data from the Excel file
+        root.df = pd.read_excel(root.file_path)
+        root.df.name = root.file_path.split('/')[-1].split('.')[0]
+
+        # Create a PandasTable
+        pt = Table(canvasBot, dataframe=root.df)
+        pt.show()
+        pt.redraw()
+
+        checkbox_font = ("Helvetica", 20)
+
+        for col in root.df.columns:
+            var = IntVar()
+            checkbox = Checkbutton(top_row_frame, bg=root.colors[2], text=col, variable=var, onvalue=1, offvalue=0, font=checkbox_font)
+            checkboxes_view.append([checkbox, var])
+
+        for col in root.df.columns:
+            var = IntVar()
+            checkbox = Checkbutton(top_row_frame, bg=root.colors[2], text=col, variable=var, onvalue=1, offvalue=0, font=checkbox_font)
+            checkboxes_split.append([checkbox, var])
+
+        for col in root.df.columns:
+            searchbar = Entry(top_row_frame_2, bg=root.colors[2], font=checkbox_font)
+            searchbar.insert(0, col)
+            searchbars.append(searchbar)
+
+
+def save_click():
+    if root.df is not None:
+
+        # Save the DataFrame to the same Excel file
+        file_path = filedialog.asksaveasfilename(defaultextension=".xlsx", initialfile=root.df.name)
+        if file_path:
+            root.df.to_excel(file_path, index=False)
+
+
+#
+#
+#
+#
+# Search click and it's checkboxes
 def search_click():
     # Disable file features
     if btnFile.cget("state") == "disabled":
@@ -36,12 +95,28 @@ def search_click():
     # Disable reformat features
     if btnReformat.cget("state") == "disabled":
         btnView.grid_forget()
+        btnSplit.grid_forget()
         btnCreate.grid_forget()
+        top_row_frame.grid_forget()
         btnReformat.config(state="normal")
-    placeholder.grid(row=0, column=0)
     btnSearch.config(state="disabled")
 
+    top_row_frame_2.grid(row=0, column=0, padx=10, pady=10)
 
+    total_items = len(searchbars)
+    num_rows = (total_items + 3) // 4  # Round up to the nearest integer
+    num_columns = (total_items + num_rows - 1) // num_rows
+    for i, searchbar in enumerate(searchbars):
+        row = i // num_columns  # Calculate the row based on the index
+        column = i % num_columns  # Calculate the column based on the index
+        searchbar.grid(row=row, column=column)
+
+
+#
+#
+#
+#
+# Reformat and it's two buttons
 def reformat_click():
     # Disable file features
     if btnFile.cget("state") == "disabled":
@@ -50,32 +125,75 @@ def reformat_click():
         btnFile.config(state="normal")
     # Disable search features
     if btnSearch.cget("state") == "disabled":
-        placeholder.grid_forget()
         btnSearch.config(state="normal")
+        top_row_frame_2.grid_forget()
     btnView.grid(row=0, column=0)
-    btnCreate.grid(row=1, column=0)
+    btnSplit.grid(row=1, column=0)
     btnReformat.config(state="disabled")
 
-
-def load_click():
-    root.filePath = filedialog.askopenfilename(initialdir="/Documents", title="Select an Excel File")
-    root.wb = excel.Excel(root.filePath)
-    for y in range(0, root.wb.columns):
-        loaded_cells.append([])
-        for x in range(0, root.wb.rows):
-            # print(root.wb.sheet.cell(row=y + 1, column=x + 1).value)
-            loaded_cells[y].append(Entry(frame, width=50))
-            loaded_cells[y][x].grid(row=y, column=x, padx=10, pady=5)
-            if root.wb.sheet.cell(row=y + 1, column=x + 1).value != None:
-                loaded_cells[y][x].insert(0, root.wb.sheet.cell(row=y+1, column=x+1).value)
-
-        frame.update_idletasks()
-        canvasBot.config(scrollregion=canvasBot.bbox("all"))
-        # Bind mouse scroll events to the canvas for vertical scrolling
-        canvasBot.bind_all("<MouseWheel>", lambda event: canvasBot.yview_scroll(-1 * (event.delta), "units"))
+    top_row_frame.grid(row=0, column=1, padx=10, pady=10, columnspan=4, rowspan=2)
 
 
-loaded_cells = []
+def view_click():
+    btnView.config(state="disabled")
+    btnCreate.grid_forget()
+    for checkbox in checkboxes_split:
+        checkbox[0].grid_forget()
+
+    total_items = len(checkboxes_view)
+    num_rows = (total_items + 3) // 4  # Round up to the nearest integer
+    num_columns = (total_items + num_rows - 1) // num_rows
+    for i, checkbox in enumerate(checkboxes_view):
+        row = i // num_columns  # Calculate the row based on the index
+        column = i % num_columns  # Calculate the column based on the index
+        checkbox[0].grid(row=row, column=column)
+
+    btnSplit.config(state="normal")
+
+
+def split_click():
+    btnSplit.config(state="disabled")
+    btnCreate.grid(row=0, column=10, rowspan=2)
+    for checkbox in checkboxes_view:
+        checkbox[0].grid_forget()
+
+    total_items = len(checkboxes_split)
+    num_rows = (total_items + 3) // 4  # Round up to the nearest integer
+    num_columns = (total_items + num_rows - 1) // num_rows
+    for i, checkbox in enumerate(checkboxes_split):
+        row = i // num_columns  # Calculate the row based on the index
+        column = i % num_columns  # Calculate the column based on the index
+        checkbox[0].grid(row=row, column=column)
+
+    btnView.config(state="normal")
+
+
+def create_click():
+    checked_columns = []
+    for checkbox in checkboxes_split:
+        if checkbox[1].get() == 1:  # Check if the checkbox is checked (value is 1)
+            print("found")
+            checked_columns.append(checkbox[0]['text'])
+
+    if not checked_columns:
+        # error_label.config(text="No columns selected.")
+        print("error")
+        return
+
+    if root.df is not None:
+        # Create a new DataFrame with only the selected columns
+        new_df = root.df[checked_columns]
+
+        # Save the new DataFrame to a new Excel file
+        file_path = filedialog.asksaveasfilename(defaultextension=".xlsx", initialfile=f"{root.df.name}_filtered")
+        if file_path:
+            new_df.to_excel(file_path, index=False)
+            error_label.config(text="File created successfully.")
+        else:
+            error_label.config(text="File creation canceled.")
+    else:
+        error_label.config(text="No data loaded to create a file.")
+
 
 # Create Canvas for navbar
 canvasTop = Canvas(root, width=1900, height=350, bg=root.colors[1])
@@ -83,44 +201,35 @@ canvasTop.pack_propagate(False)
 canvasTop.grid_propagate(False)
 
 canvasBot = Canvas(root, width=1900, height=600, bg=root.colors[1])
-x_scrollbar = Scrollbar(canvasBot, orient="horizontal")
-y_scrollbar = Scrollbar(canvasBot, orient="vertical")
-frame = Frame(canvasBot, width=1887, height=587)
-
-canvasBot.config(xscrollcommand=x_scrollbar.set, yscrollcommand=y_scrollbar.set)
 canvasBot.pack_propagate(False)
 canvasBot.grid_propagate(False)
-frame.pack_propagate(False)
-frame.grid_propagate(False)
 
 # Creates File and its buttons
 btnFile = Button(root, text="File", padx=60, pady=10, bg=root.colors[1], command=file_click)
 btnLoad = Button(canvasTop, text="Load", width=20, height=11, bg=root.colors[2], command=load_click)
-btnSave = Button(canvasTop, text="Save", width=20, height=11, bg=root.colors[2])
+btnSave = Button(canvasTop, text="Save", width=20, height=11, bg=root.colors[2], command=save_click)
 frameFile = LabelFrame(root, padx=800, pady=20, fg='#E4E6EB', bg=root.colors[2])
 
 # Creates Search and its entries
 btnSearch = Button(root, text="Search", padx=60, pady=10, bg=root.colors[1], command=search_click)
-placeholder = Entry(canvasTop, width=50)
+top_row_frame_2 = Frame(canvasTop, bg=root.colors[1])
 
 # Creates Reformat and its buttons
 btnReformat = Button(root, text="Reformat", padx=60, pady=10, bg=root.colors[1], command=reformat_click)
-btnView = Button(canvasTop, text="View", width=20, height=11, bg=root.colors[2])
-btnCreate = Button(canvasTop, text="Create", width=20, height=11, bg=root.colors[2])
+btnView = Button(canvasTop, text="View", width=20, height=11, bg=root.colors[2], command=view_click)
+btnSplit = Button(canvasTop, text="Split", width=20, height=11, bg=root.colors[2], command=split_click)
+btnCreate = Button(canvasTop, text="Create", width=20, height=22, bg=root.colors[2], command=create_click)
+top_row_frame = Frame(canvasTop, bg=root.colors[1])
 
 # Puts everything onto the grid
 btnFile.grid(row=0, column=0)
 btnSearch.grid(row=0, column=1)
 btnReformat.grid(row=0, column=2)
 canvasTop.grid(row=1, column=0, padx=10, pady=10, columnspan=4)
-canvasBot.grid(row=2, column=0, padx=10, pady=10, columnspan=4)
-x_scrollbar.pack(side="bottom", fill="x")
-y_scrollbar.pack(side="right", fill="y")
 
-# For Scrollbar and Canvas Frame
-x_scrollbar.config(command=canvasBot.xview)
-y_scrollbar.config(command=canvasBot.yview)
-canvasBot.create_window((0, 0), window=frame, anchor="nw")
+error_label = Label(canvasBot, text="", foreground="red")
+error_label.grid(row=1, column=0)
+canvasBot.grid(row=2, column=0, padx=10, pady=10, columnspan=4)
 
 file_click()
 root.mainloop()
--- src/test2.py
+++ src/test2.py
@@ -1,32 +1,41 @@
-#Import the required library
-from tkinter import *
-#Create an instance of tkinter frame or window
-win = Tk()
-#Define the geometry
-win.geometry("750x400")
-#Create a Frame
-frame= Frame(win)
-def close():
-   win.destroy()
-#Create a Label widget in the frame
-text= Label(frame, text= "Register", font= ('Helvetica bold', 14))
-text.pack(pady=20)
-#ADDING A SCROLLBAR
-myscrollbar=Scrollbar(frame,orient="vertical")
-myscrollbar.pack(side="right",fill="y")
-#Add Entry Widgets
-Label(frame, text= "Username").pack()
-username= Entry(frame, width= 20)
-username.pack()
-Label(frame, text= "password").pack()
-password= Entry(frame, show="*", width= 15)
-password.pack()
-Label(frame, text= "Email Id").pack()
-email= Entry(frame, width= 15)
-email.pack()
-#Create widget in the frame
-button= Button(frame, text= "Close",font= ('Helvetica bold',14),
-command= close)
-button.pack(pady=20)
-frame.pack()
-win.mainloop()
+import tkinter as tk
+from tkinter import ttk
+import pandas as pd
+from pandastable import Table
+
+
+def display_excel_file():
+    file_path = file_entry.get()
+    try:
+        df = pd.read_excel(file_path)
+        display_table(df)
+    except Exception as e:
+        error_label.config(text=f"Error: {str(e)}")
+
+
+def display_table(dataframe):
+    if hasattr(root, 'table_frame'):
+        root.table_frame.destroy()
+
+    root.table_frame = ttk.Frame(root)
+    root.table_frame.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
+
+    table = Table(root.table_frame, dataframe=dataframe, showtoolbar=True, showstatusbar=True)
+    table.show()
+
+
+root = tk.Tk()
+root.title("Excel Viewer")
+
+file_label = ttk.Label(root, text="Enter Excel File Path:")
+file_label.pack(pady=10)
+file_entry = ttk.Entry(root)
+file_entry.pack(pady=10)
+
+display_button = ttk.Button(root, text="Display Excel File", command=display_excel_file)
+display_button.pack(pady=10)
+
+error_label = ttk.Label(root, text="", foreground="red")
+error_label.pack()
+
+root.mainloop()
