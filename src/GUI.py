5622296acb9ca047d1c0d79de38126f9e3d2519a
AmarMcLean <114834915+AmarMcLean@users.noreply.github.com>
2023-10-30 00:27:36 -0400

    10/28/23 Created GUI for Form Style Entry
    Bug fixed:
    When clicking View or Split without a file, they don't become clickable again

--- src/GUI.py
+++ src/GUI.py
@@ -16,16 +16,17 @@ root.df = None
 checkboxes_view = []
 checkboxes_split = []
 searchbars = []
+entries = []
 root.file_path = None
 
 root.configure(bg=root.colors[0])
 
 
-#
-#
-#
+#################################################
 #
 # File tab and the load button and save button
+#
+#################################################
 def file_click():
     # Disable search features
     if btnSearch.cget("state") == "disabled":
@@ -57,15 +58,25 @@ def load_click():
         pt.redraw()
 
         checkbox_font = ("Helvetica", 20)
+        btnEntry.config(state="normal")
+
+        if checkboxes_view:
+            checkboxes_view.clear()
+        if checkboxes_split:
+            checkboxes_split.clear()
+        if searchbars:
+            searchbars.clear()
 
         for col in root.df.columns:
             var = IntVar()
-            checkbox = Checkbutton(top_row_frame, bg=root.colors[2], text=col, variable=var, onvalue=1, offvalue=0, font=checkbox_font)
+            checkbox = Checkbutton(top_row_frame, bg=root.colors[2], text=col, variable=var, onvalue=1, offvalue=0,
+                                   font=checkbox_font)
             checkboxes_view.append([checkbox, var])
 
         for col in root.df.columns:
             var = IntVar()
-            checkbox = Checkbutton(top_row_frame, bg=root.colors[2], text=col, variable=var, onvalue=1, offvalue=0, font=checkbox_font)
+            checkbox = Checkbutton(top_row_frame, bg=root.colors[2], text=col, variable=var, onvalue=1, offvalue=0,
+                                   font=checkbox_font)
             checkboxes_split.append([checkbox, var])
 
         for col in root.df.columns:
@@ -83,11 +94,11 @@ def save_click():
             root.df.to_excel(file_path, index=False)
 
 
-#
-#
-#
+#################################################
 #
 # Search click and it's checkboxes
+#
+#################################################
 def search_click():
     # Disable file features
     if btnFile.cget("state") == "disabled":
@@ -114,11 +125,11 @@ def search_click():
         searchbar.grid(row=row, column=column)
 
 
-#
-#
-#
+#################################################
 #
 # Reformat and it's two buttons
+#
+#################################################
 def reformat_click():
     # Disable file features
     if btnFile.cget("state") == "disabled":
@@ -143,12 +154,13 @@ def view_click():
         checkbox[0].grid_forget()
 
     total_items = len(checkboxes_view)
-    num_rows = (total_items + 3) // 4  # Round up to the nearest integer
-    num_columns = (total_items + num_rows - 1) // num_rows
-    for i, checkbox in enumerate(checkboxes_view):
-        row = i // num_columns  # Calculate the row based on the index
-        column = i % num_columns  # Calculate the column based on the index
-        checkbox[0].grid(row=row, column=column)
+    if total_items != 0:
+        num_rows = (total_items + 3) // 4  # Round up to the nearest integer
+        num_columns = (total_items + num_rows - 1) // num_rows
+        for i, checkbox in enumerate(checkboxes_view):
+            row = i // num_columns  # Calculate the row based on the index
+            column = i % num_columns  # Calculate the column based on the index
+            checkbox[0].grid(row=row, column=column)
 
     btnSplit.config(state="normal")
 
@@ -160,12 +172,13 @@ def split_click():
         checkbox[0].grid_forget()
 
     total_items = len(checkboxes_split)
-    num_rows = (total_items + 3) // 4  # Round up to the nearest integer
-    num_columns = (total_items + num_rows - 1) // num_rows
-    for i, checkbox in enumerate(checkboxes_split):
-        row = i // num_columns  # Calculate the row based on the index
-        column = i % num_columns  # Calculate the column based on the index
-        checkbox[0].grid(row=row, column=column)
+    if total_items != 0:
+        num_rows = (total_items + 3) // 4  # Round up to the nearest integer
+        num_columns = (total_items + num_rows - 1) // num_rows
+        for i, checkbox in enumerate(checkboxes_split):
+            row = i // num_columns  # Calculate the row based on the index
+            column = i % num_columns  # Calculate the column based on the index
+            checkbox[0].grid(row=row, column=column)
 
     btnView.config(state="normal")
 
@@ -174,12 +187,12 @@ def create_click():
     checked_columns = []
     for checkbox in checkboxes_split:
         if checkbox[1].get() == 1:  # Check if the checkbox is checked (value is 1)
-            print("found")
+            print("Column found")
             checked_columns.append(checkbox[0]['text'])
 
     if not checked_columns:
         # error_label.config(text="No columns selected.")
-        print("error")
+        print("No columns loaded or checked")
         return
 
     if root.df is not None:
@@ -190,21 +203,78 @@ def create_click():
         file_path = filedialog.asksaveasfilename(defaultextension=".xlsx", initialfile=f"{root.df.name}_filtered")
         if file_path:
             new_df.to_excel(file_path, index=False)
-            error_label.config(text="File created successfully.")
-        else:
-            error_label.config(text="File creation canceled.")
-    else:
-        error_label.config(text="No data loaded to create a file.")
 
 
-# Create Canvas for navbar
+#################################################
+#
+# Entry button and it's Add button
+#
+#################################################
+def entry_click():
+    entry_window = Toplevel(root)
+    entry_window.title("Form Entry")
+    entry_window.geometry("500x700")
+
+    # Make Frame
+    main_frame = Frame(entry_window)
+    main_frame.pack(fill=BOTH, expand=1)
+
+    # Make Canvas
+    my_canvas = Canvas(main_frame)
+    my_canvas.pack(side=LEFT, fill=BOTH, expand=1)
+
+    # Make Scroll bar and put it in Frame
+    my_scrollbar = Scrollbar(main_frame, orient=VERTICAL, command=my_canvas.yview)
+    my_scrollbar.pack(side=RIGHT, fill=Y)
+
+    # Configure Canvas
+    my_canvas.configure(yscrollcommand=my_scrollbar.set)
+    my_canvas.bind('<Configure>', lambda e: my_canvas.configure(scrollregion=my_canvas.bbox("all")))
+
+    # Make Second Frame
+    frame = Frame(my_canvas)
+
+    my_canvas.create_window((0,0), window=frame, anchor="nw")
+
+    # Make add button
+    btn_add = Button(frame, text="Add", padx=45, pady=5, bg=root.colors[1], command=add_click)
+    num_col = 0
+
+    if entries:
+        entries.clear()
+
+    for col in root.df.columns:
+        label = Label(frame, text=col, padx=10, pady=10)
+        entry = Entry(frame, bg=root.colors[2])
+        var = IntVar()
+        checkbox = Checkbutton(frame, variable=var, onvalue=1, offvalue=0)
+
+        label.grid(row=num_col, column=0)
+        entry.grid(row=num_col, column=1)
+        checkbox.grid(row=num_col, column=2)
+        entries.append([entry, checkbox])
+
+        num_col += 1
+
+    btn_add.grid(row=num_col, column=1)
+
+
+def add_click():
+    # Might have to update and redraw table?
+    None
+
+
+# Create Top Canvas
 canvasTop = Canvas(root, width=1900, height=350, bg=root.colors[1])
 canvasTop.pack_propagate(False)
 canvasTop.grid_propagate(False)
 
+# Create Bottom Canvas
 canvasBot = Canvas(root, width=1900, height=600, bg=root.colors[1])
 canvasBot.pack_propagate(False)
 canvasBot.grid_propagate(False)
+
+# Create Entry Window
 
 # Creates File and its buttons
 btnFile = Button(root, text="File", padx=60, pady=10, bg=root.colors[1], command=file_click)
@@ -223,14 +293,20 @@ btnSplit = Button(canvasTop, text="Split", width=20, height=11, bg=root.colors[2
 btnCreate = Button(canvasTop, text="Create", width=20, height=22, bg=root.colors[2], command=create_click)
 top_row_frame = Frame(canvasTop, bg=root.colors[1])
 
-# Puts everything onto the grid
+# Creates Entry Button
+btnEntry = Button(root, text="Entry", padx=60, pady=10, bg=root.colors[1], command=entry_click)
+
+# Puts navbar buttons onto grid
 btnFile.grid(row=0, column=0)
 btnSearch.grid(row=0, column=1)
 btnReformat.grid(row=0, column=2)
+btnEntry.grid(row=0, column=3)
+btnEntry.config(state="disabled")
+
+# Puts top canvas onto grid
 canvasTop.grid(row=1, column=0, padx=10, pady=10, columnspan=4)
 
-error_label = Label(canvasBot, text="", foreground="red")
-error_label.grid(row=1, column=0)
+# Puts bot canvas onto grid
 canvasBot.grid(row=2, column=0, padx=10, pady=10, columnspan=4)
 
 file_click()
