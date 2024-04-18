a070d1d2de95d51fb11276b7a758d17fc90a8077
AmarMcLean <114834915+AmarMcLean@users.noreply.github.com>
2024-04-18 19:16:42 -0400

    More SpeederUpper

--- Work/KualiSpeederUpper.py
+++ Work/KualiSpeederUpper.py
@@ -1,20 +1,18 @@
 import openpyxl
 
 if __name__ == '__main__':
-    file_path = "2020Kuali-M33.xlsx"
+    file_path = "2021Kuali-MegaFile-M33-35.xlsx"
     sheet_old = openpyxl.load_workbook(file_path)
-    inactive_sheet = sheet_old
     sheet_old = sheet_old.active
-
-    sheet_new = []
 
     tuples = tuple(sheet_old.rows)
     for t1 in range(len(tuples)):
         found = False
-        for t2 in range(len(tuples)):
-            if tuples[t1][0].value == tuples[t2][1].value:
-                found = True
-                # print(tuples[t1][0].value + " " + tuples[t2][1].value)
-                print(tuples[t2][3].value + " " + tuples[t2][2].value)
-        if not found:
-            print("-")
+        if tuples[t1][0].value is not None:
+            for t2 in range(len(tuples)):
+                if tuples[t1][0].value == tuples[t2][1].value:
+                    found = True
+                     # print(tuples[t1][0].value + " " + tuples[t2][1].value)
+                    print(tuples[t2][3].value + " " + tuples[t2][2].value)
+            if not found:
+                print("-")
